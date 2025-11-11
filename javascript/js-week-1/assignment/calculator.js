bf52c2b99aa62eebf4e468ac21d3a35787ac5959
ahmedminhaj <ahmedminhaj44@gmail.com>
2025-11-11 19:42:22 +0100

    GoodBoy-OldBoy: calculate the dog age in human year and also in dog year

--- javascript/js-week-1/assignment/calculator.js
+++ javascript/js-week-1/assignment/calculator.js
@@ -3,8 +3,22 @@ const yearFuture = 2030;
 
 const age = 2030 - 1999;
 
-const message = `You will be ${age} years old in ${yearFuture}.`
+const message = `You will be ${age} years old in ${yearFuture}.`;
 
-console.log("_____Age-ify_____")
+console.log("_____Age-ify_____");
 console.log(message);
+
+const dogYearBirth = 2019;
+const dogYearFuture = 2027;
+const dogYear = 7;
+
+const shouldShowResultInDogYears = false;
+
+let dogAge = 2027 - 2019;
+dogAge = dogAge * (shouldShowResultInDogYears ? dogYear : 1);
+
+const dogMessage = `Your dog will be ${dogAge} ${shouldShowResultInDogYears ? "dog" : "human"} years old in ${dogYearFuture}`;
+
+console.log("_____GoodBoy-OldBoy_______");
+console.log(dogMessage);
 
