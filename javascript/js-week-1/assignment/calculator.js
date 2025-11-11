62e0ba36bc11103a4ac207e5965c9e7970cd06df
ahmedminhaj <ahmedminhaj44@gmail.com>
2025-11-11 20:39:04 +0100

    Housey-pricey: calculate the house rent and estimated price

--- javascript/js-week-1/assignment/calculator.js
+++ javascript/js-week-1/assignment/calculator.js
@@ -22,3 +22,24 @@ const dogMessage = `Your dog will be ${dogAge} ${shouldShowResultInDogYears ? "d
 console.log("_____GoodBoy-OldBoy_______");
 console.log(dogMessage);
 
+console.log("_____Housey-Pricey_____")
+const peterHouseVolumeInM2 = 8 * 10 * 10;
+const peterGardenSize = 100;
+const peterRentPaid = 2500000;
+const juliaHouseVolumeInM2 = 5 * 11 * 8;
+const juliaGardenSize = 70;
+const juliaRentPaid = 1000000;
+
+function rentCalculate(volumeInMeters, gardenSizeInM2) {
+    return volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+}
+
+const peterEstimatedRent = rentCalculate(peterHouseVolumeInM2, peterGardenSize);
+const juliaEstimatedRent = rentCalculate(juliaHouseVolumeInM2, juliaGardenSize);
+
+function priceEstimation(name, paidRend, estimatedRent) {
+    console.log(`${name} is paying  ${(paidRend > estimatedRent) ? "much" : "little"}.`) 
+}
+
+priceEstimation("Peter", peterRentPaid, peterEstimatedRent);
+priceEstimation("Julia", juliaRentPaid, juliaEstimatedRent);
