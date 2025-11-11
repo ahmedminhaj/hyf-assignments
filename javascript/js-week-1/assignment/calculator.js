4d7402fcb8d791b1791d2b130cb011e7c8935bd1
ahmedminhaj <ahmedminhaj44@gmail.com>
2025-11-11 19:23:55 +0100

    Age-ify: calculate the future age

---
+++ javascript/js-week-1/assignment/calculator.js
@@ -0,0 +1,10 @@
+const yearOfBirth = 1999;
+const yearFuture = 2030;
+
+const age = 2030 - 1999;
+
+const message = `You will be ${age} years old in ${yearFuture}.`
+
+console.log("_____Age-ify_____")
+console.log(message);
+
