72a340ae3adfa6a769f5a92487b7d1e7044531f6
ahmedminhaj <ahmedminhaj44@gmail.com>
2025-11-19 04:04:38 +0100

    event-day: finding the event week day with a function

--- javascript/js-week-2/exercise/script.js
+++ javascript/js-week-2/exercise/script.js
@@ -13,3 +13,24 @@ console.log(getFullName("Hughes", "Jack"))
 for woman, we can take a new param genderRole it could be boolean/string which will let us know about the person's gender role 
 with the value of genderRole we can change the title for woman in formal name
 */
+
+//Event Date
+console.log("----Find event Day----")
+const getEventWeekDay = (remainingDay) => {
+    const dayNames = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    
+    const todayDate = new Date();
+    const todayInNumber = todayDate.getDay(); // get the day in 0-6 
+    const eventDayInNumber = todayInNumber + remainingDay % 7; 
+    const eventWeekDay = dayNames[eventDayInNumber % 6]; 
+    return eventWeekDay;
+}
+console.log(`Event is ${getEventWeekDay(11)}`);
